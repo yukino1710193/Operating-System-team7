ba703ca56f1a6b9c397ebe9efd396f65d8560431
yukino1710193 <sasukevp1959@gmail.com>
2024-12-28 17:39:02 +0700

    Pinku-chan add MD5 checksum method

--- client.cpp
+++ client.cpp
@@ -1,6 +1,8 @@
 // client.cpp - SSL/TLS enabled client
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <iomanip>
 #include <cstring>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -8,6 +10,7 @@
 #include <csignal>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
+#include <openssl/md5.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
@@ -19,22 +22,26 @@ int sock;
 SSL_CTX *ssl_ctx;
 SSL *ssl;
 
-void init_openssl() {
+void init_openssl()
+{
     SSL_load_error_strings();
     OpenSSL_add_ssl_algorithms();
 }
 
-void cleanup_openssl() {
+void cleanup_openssl()
+{
     EVP_cleanup();
 }
 
-SSL_CTX *create_context() {
+SSL_CTX *create_context()
+{
     const SSL_METHOD *method;
     SSL_CTX *ctx;
 
     method = SSLv23_client_method();
     ctx = SSL_CTX_new(method);
-    if (!ctx) {
+    if (!ctx)
+    {
         cerr << "Unable to create SSL context" << endl;
         ERR_print_errors_fp(stderr);
         exit(EXIT_FAILURE);
@@ -43,9 +50,11 @@ SSL_CTX *create_context() {
     return ctx;
 }
 
-void configure_context(SSL_CTX *ctx) {
+void configure_context(SSL_CTX *ctx)
+{
     // Load the CA certificate (server.crt)
-    if (SSL_CTX_load_verify_locations(ctx, "server.crt", NULL) <= 0) {
+    if (SSL_CTX_load_verify_locations(ctx, "server.crt", NULL) <= 0)
+    {
         ERR_print_errors_fp(stderr);
         exit(EXIT_FAILURE);
     }
@@ -55,13 +64,34 @@ void configure_context(SSL_CTX *ctx) {
     SSL_CTX_set_verify_depth(ctx, 1);
 }
 
+string calculate_md5(const string &filename)
+{
+    ifstream file(filename, ios::binary);
+    if (!file.is_open())
+        return "";
+    MD5_CTX md5_ctx;
+    MD5_Init(&md5_ctx);
+    char buffer[BUFFER_SIZE];
+    while (file.read(buffer, sizeof(buffer)))
+    {
+        MD5_Update(&md5_ctx, buffer, file.gcount());
+    }
+    unsigned char result[MD5_DIGEST_LENGTH];
+    MD5_Final(result, &md5_ctx);
+    stringstream ss;
+    for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)
+        ss << hex << setw(2) << setfill('0') << (int)result[i];
+    return ss.str();
+}
+
 void list_files();
 void download_file();
 void upload_file();
 void exit_client();
 void handle_signal(int signum);
 
-int main() {
+int main()
+{
     struct sockaddr_in server_addr;
 
     init_openssl();
@@ -71,12 +101,14 @@ int main() {
     cout << "Enter server IP (default " << IP_SERVER << "): ";
     string input_ip;
     getline(cin, input_ip);
-    if (!input_ip.empty()) {
+    if (!input_ip.empty())
+    {
         IP_SERVER = input_ip;
     }
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
+    if (sock < 0)
+    {
         cerr << "Socket creation failed!\n";
         return -1;
     }
@@ -85,7 +117,8 @@ int main() {
     server_addr.sin_port = htons(PORT);
     inet_pton(AF_INET, IP_SERVER.c_str(), &server_addr.sin_addr);
 
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
         cerr << "Connection to server failed!\n";
         return -1;
     }
@@ -93,7 +126,8 @@ int main() {
     ssl = SSL_new(ssl_ctx);
     SSL_set_fd(ssl, sock);
 
-    if (SSL_connect(ssl) <= 0) {
+    if (SSL_connect(ssl) <= 0)
+    {
         ERR_print_errors_fp(stderr);
         return -1;
     }
@@ -102,13 +136,15 @@ int main() {
 
     cout << "Connected to server securely.\n";
 
-    while (true) {
+    while (true)
+    {
         cout << "\n1. List files\n2. Download file\n3. Upload file\n4. Exit\nChoose: ";
         char choice;
         cin >> choice;
         cin.ignore();
 
-        switch (choice) {
+        switch (choice)
+        {
         case '1':
             list_files();
             break;
@@ -127,7 +163,8 @@ int main() {
     }
 }
 
-void list_files() {
+void list_files()
+{
     char choice = '1';
     SSL_write(ssl, &choice, sizeof(choice));
 
@@ -137,9 +174,11 @@ void list_files() {
     char buffer[BUFFER_SIZE];
     size_t total_bytes_received = 0;
 
-    while (total_bytes_received < list_size) {
+    while (total_bytes_received < list_size)
+    {
         ssize_t bytes_received = SSL_read(ssl, buffer, std::min(static_cast<size_t>(BUFFER_SIZE), list_size - total_bytes_received));
-        if (bytes_received <= 0) {
+        if (bytes_received <= 0)
+        {
             cerr << "Error receiving file list.\n";
             return;
         }
@@ -149,7 +188,8 @@ void list_files() {
     cout << "File list received successfully.\n";
 }
 
-void download_file() {
+void download_file()
+{
     cout << "Enter filename to download: ";
     string filename;
     cin >> filename;
@@ -159,19 +199,28 @@ void download_file() {
     SSL_write(ssl, filename.c_str(), filename.size() + 1);
 
     size_t file_size;
-    SSL_read(ssl, &file_size, sizeof(file_size)); // Receive size of the file
+    SSL_read(ssl, &file_size, sizeof(file_size));
+
+    if (file_size == 0)
+    {
+        cerr << "Error: File not found on server.\n";
+        return;
+    }
 
     ofstream file(filename, ios::binary);
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         cerr << "Failed to create file!\n";
         return;
     }
 
     char buffer[BUFFER_SIZE];
     size_t total_bytes_received = 0;
-    while (total_bytes_received < file_size) {
+    while (total_bytes_received < file_size)
+    {
         ssize_t bytes_received = SSL_read(ssl, buffer, std::min(static_cast<size_t>(BUFFER_SIZE), file_size - total_bytes_received));
-        if (bytes_received <= 0) {
+        if (bytes_received <= 0)
+        {
             cerr << "Error receiving file data.\n";
             break;
         }
@@ -179,16 +228,37 @@ void download_file() {
         total_bytes_received += bytes_received;
     }
     file.close();
+
+    // Nhận checksum từ server
+    char server_checksum[MD5_DIGEST_LENGTH * 2 + 1] = {0};
+    SSL_read(ssl, server_checksum, sizeof(server_checksum));
+    string local_checksum = calculate_md5(filename);
+
     cout << "File downloaded successfully. Total bytes received: " << total_bytes_received << "\n";
+
+    // So sánh và in checksum
+    cout << "Checksum from server: " << server_checksum << "\n";
+    cout << "Checksum calculated locally: " << local_checksum << "\n";
+
+    if (local_checksum == string(server_checksum))
+    {
+        cout << "Checksum matched! File integrity verified.\n";
+    }
+    else
+    {
+        cerr << "Checksum mismatch! File may be corrupted.\n";
+    }
 }
 
-void upload_file() {
+void upload_file()
+{
     cout << "Enter filename to upload: ";
     string filename;
     cin >> filename;
 
     ifstream file(filename, ios::binary);
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         cerr << "File not found!\n";
         return;
     }
@@ -204,19 +274,26 @@ void upload_file() {
 
     char buffer[BUFFER_SIZE];
     size_t total_bytes_sent = 0;
-    while (file.read(buffer, sizeof(buffer))) {
+    while (file.read(buffer, sizeof(buffer)))
+    {
         total_bytes_sent += SSL_write(ssl, buffer, file.gcount());
     }
 
-    if (file.gcount() > 0) {
+    if (file.gcount() > 0)
+    {
         total_bytes_sent += SSL_write(ssl, buffer, file.gcount());
     }
+
+    // Gửi checksum của file
+    string checksum = calculate_md5(filename);
+    SSL_write(ssl, checksum.c_str(), checksum.size() + 1);
 
     file.close();
     cout << "File uploaded successfully. Total bytes sent: " << total_bytes_sent << "\n";
 }
 
-void exit_client() {
+void exit_client()
+{
     char choice = '4';
     SSL_write(ssl, &choice, sizeof(choice));
     SSL_shutdown(ssl);
@@ -227,7 +304,8 @@ void exit_client() {
     cout << "Disconnected from server.\n";
 }
 
-void handle_signal(int signum) {
+void handle_signal(int signum)
+{
     exit_client();
     exit(0);
 }
--- server.cpp
+++ server.cpp
@@ -1,15 +1,19 @@
 // server.cpp - SSL/TLS enabled server
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <iomanip>
 #include <cstring>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <thread>
 #include <filesystem>
 #include <csignal>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
+#include <openssl/md5.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
@@ -21,22 +25,46 @@ string dir = "./cloud/"; // Directory for shared files
 int sockfd;
 SSL_CTX *ssl_ctx;
 
-void init_openssl() {
+string calculate_md5(const string &filename)
+{
+    ifstream file(filename, ios::binary);
+    if (!file.is_open())
+        return "";
+    MD5_CTX md5_ctx;
+    MD5_Init(&md5_ctx);
+    char buffer[BUFFER_SIZE];
+    while (file.read(buffer, sizeof(buffer)))
+    {
+        MD5_Update(&md5_ctx, buffer, file.gcount());
+    }
+    unsigned char result[MD5_DIGEST_LENGTH];
+    MD5_Final(result, &md5_ctx);
+    stringstream ss;
+    for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)
+        ss << hex << setw(2) << setfill('0') << (int)result[i];
+    return ss.str();
+}
+
+void init_openssl()
+{
     SSL_load_error_strings();
     OpenSSL_add_ssl_algorithms();
 }
 
-void cleanup_openssl() {
+void cleanup_openssl()
+{
     EVP_cleanup();
 }
 
-SSL_CTX *create_context() {
+SSL_CTX *create_context()
+{
     const SSL_METHOD *method;
     SSL_CTX *ctx;
 
     method = SSLv23_server_method();
     ctx = SSL_CTX_new(method);
-    if (!ctx) {
+    if (!ctx)
+    {
         cerr << "Unable to create SSL context" << endl;
         ERR_print_errors_fp(stderr);
         exit(EXIT_FAILURE);
@@ -45,14 +73,17 @@ SSL_CTX *create_context() {
     return ctx;
 }
 
-void configure_context(SSL_CTX *ctx) {
+void configure_context(SSL_CTX *ctx)
+{
     // Set the key and cert
-    if (SSL_CTX_use_certificate_file(ctx, "server.crt", SSL_FILETYPE_PEM) <= 0) {
+    if (SSL_CTX_use_certificate_file(ctx, "server.crt", SSL_FILETYPE_PEM) <= 0)
+    {
         ERR_print_errors_fp(stderr);
         exit(EXIT_FAILURE);
     }
 
-    if (SSL_CTX_use_PrivateKey_file(ctx, "server.key", SSL_FILETYPE_PEM) <= 0) {
+    if (SSL_CTX_use_PrivateKey_file(ctx, "server.key", SSL_FILETYPE_PEM) <= 0)
+    {
         ERR_print_errors_fp(stderr);
         exit(EXIT_FAILURE);
     }
@@ -63,7 +94,8 @@ void send_file_list(SSL *ssl);
 void send_file(SSL *ssl, const string &filename);
 void receive_file(SSL *ssl, const string &filename);
 
-void handle_signal(int signum) {
+void handle_signal(int signum)
+{
     cout << "\nShutting down server...\n";
     close(sockfd);
     SSL_CTX_free(ssl_ctx);
@@ -71,7 +103,8 @@ void handle_signal(int signum) {
     exit(0);
 }
 
-int main() {
+int main()
+{
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
 
@@ -80,7 +113,8 @@ int main() {
     configure_context(ssl_ctx);
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
+    if (sockfd == -1)
+    {
         cerr << "Socket creation failed!\n";
         return -1;
     }
@@ -90,23 +124,27 @@ int main() {
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
-    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
         cerr << "Binding failed!\n";
         return -1;
     }
 
     signal(SIGINT, handle_signal);
 
-    if (listen(sockfd, 10) < 0) {
+    if (listen(sockfd, 10) < 0)
+    {
         cerr << "Listening failed!\n";
         return -1;
     }
 
     cout << "Server is listening on port " << PORT << "...\n";
 
-    while (true) {
+    while (true)
+    {
         int new_sock = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
-        if (new_sock < 0) {
+        if (new_sock < 0)
+        {
             cerr << "Connection acceptance failed!\n";
             continue;
         }
@@ -114,7 +152,8 @@ int main() {
         SSL *ssl = SSL_new(ssl_ctx);
         SSL_set_fd(ssl, new_sock);
 
-        if (SSL_accept(ssl) <= 0) {
+        if (SSL_accept(ssl) <= 0)
+        {
             ERR_print_errors_fp(stderr);
             close(new_sock);
             continue;
@@ -122,10 +161,10 @@ int main() {
 
         cout << "New connection accepted from client.\n";
 
-        thread client_thread([ssl]() {
+        thread client_thread([ssl]()
+                             {
             handle_client(ssl);
-            SSL_free(ssl);
-        });
+            SSL_free(ssl); });
         client_thread.detach();
     }
 
@@ -135,22 +174,27 @@ int main() {
     return 0;
 }
 
-void handle_client(SSL *ssl) {
+void handle_client(SSL *ssl)
+{
     char choice;
-    while (SSL_read(ssl, &choice, sizeof(choice)) > 0) {
-        switch (choice) {
+    while (SSL_read(ssl, &choice, sizeof(choice)) > 0)
+    {
+        switch (choice)
+        {
         case '1':
             cout << "Client requested file list.\n";
             send_file_list(ssl);
             break;
-        case '2': {
+        case '2':
+        {
             char filename[BUFFER_SIZE];
             SSL_read(ssl, filename, BUFFER_SIZE);
             cout << "Client requested to download file: " << filename << "\n";
             send_file(ssl, dir + filename);
             break;
         }
-        case '3': {
+        case '3':
+        {
             char filename[BUFFER_SIZE];
             SSL_read(ssl, filename, BUFFER_SIZE);
             cout << "Client is uploading file: " << filename << "\n";
@@ -168,9 +212,11 @@ void handle_client(SSL *ssl) {
     SSL_shutdown(ssl);
 }
 
-void send_file_list(SSL *ssl) {
+void send_file_list(SSL *ssl)
+{
     string file_list;
-    for (const auto &entry : fs::directory_iterator(dir)) {
+    for (const auto &entry : fs::directory_iterator(dir))
+    {
         file_list += entry.path().filename().string() + "\n";
     }
 
@@ -181,9 +227,11 @@ void send_file_list(SSL *ssl) {
     cout << "Sent file list to client. Total bytes sent: " << list_size << "\n";
 }
 
-void send_file(SSL *ssl, const string &filename) {
+void send_file(SSL *ssl, const string &filename)
+{
     ifstream file(filename, ios::binary);
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         cerr << "File not found: " << filename << "\n";
         size_t error_size = 0;
         SSL_write(ssl, &error_size, sizeof(error_size));
@@ -197,23 +245,29 @@ void send_file(SSL *ssl, const string &filename) {
     SSL_write(ssl, &file_size, sizeof(file_size));
 
     char buffer[BUFFER_SIZE];
-    size_t total_bytes_sent = 0;
-
-    while (file.read(buffer, sizeof(buffer))) {
-        total_bytes_sent += SSL_write(ssl, buffer, file.gcount());
+    while (file.read(buffer, sizeof(buffer)))
+    {
+        SSL_write(ssl, buffer, file.gcount());
+    }
+    if (file.gcount() > 0)
+    {
+        SSL_write(ssl, buffer, file.gcount());
     }
 
-    if (file.gcount() > 0) {
-        total_bytes_sent += SSL_write(ssl, buffer, file.gcount());
-    }
+    string checksum = calculate_md5(filename);
+    SSL_write(ssl, checksum.c_str(), checksum.size() + 1);
 
-    cout << "File " << filename << " sent successfully. Total bytes sent: " << total_bytes_sent << "\n";
+    // Thêm in checksum trên server
+    cout << "File sent: " << filename << " (size: " << file_size
+         << " bytes, checksum: " << checksum << ")\n";
     file.close();
 }
 
-void receive_file(SSL *ssl, const string &filename) {
+void receive_file(SSL *ssl, const string &filename)
+{
     ofstream file(filename, ios::binary);
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         cerr << "Failed to open file for writing: " << filename << "\n";
         return;
     }
@@ -223,17 +277,58 @@ void receive_file(SSL *ssl, const string &filename) {
 
     char buffer[BUFFER_SIZE];
     size_t total_bytes_received = 0;
-
-    while (total_bytes_received < file_size) {
-        ssize_t bytes_received = SSL_read(ssl, buffer, std::min(static_cast<size_t>(BUFFER_SIZE), file_size - total_bytes_received));
-        if (bytes_received <= 0) {
-            cerr << "Error receiving file data.\n";
+    while (file_size > 0)
+    {
+        ssize_t bytes_received = SSL_read(ssl, buffer, std::min<size_t>(BUFFER_SIZE, file_size));
+        if (bytes_received <= 0)
             break;
-        }
         file.write(buffer, bytes_received);
+        file_size -= bytes_received;
         total_bytes_received += bytes_received;
     }
-
-    cout << "File " << filename << " received successfully. Total bytes received: " << total_bytes_received << "\n";
     file.close();
+
+    // Nhận checksum từ client
+    char client_checksum[MD5_DIGEST_LENGTH * 2 + 1] = {0};
+    SSL_read(ssl, client_checksum, sizeof(client_checksum));
+
+    // Tính checksum của file vừa lưu
+    string server_checksum = calculate_md5(filename);
+
+    // So sánh checksum
+    if (server_checksum == string(client_checksum))
+    {
+        cout << "File received successfully. Checksum verified: " << server_checksum << "\n";
+    }
+    else
+    {
+        cerr << "Checksum mismatch for file: " << filename << "\n";
+        cerr << "Expected: " << client_checksum << "\n";
+        cerr << "Received: " << server_checksum << "\n";
+    }
+
+    cout << "File received: " << filename << " (size: " << total_bytes_received << " bytes)\n";
+}
+
+void delete_file(SSL *ssl)
+{
+    char filename[BUFFER_SIZE];
+    SSL_read(ssl, filename, BUFFER_SIZE);
+    string filepath = dir + filename;
+
+    if (fs::exists(filepath) && fs::is_regular_file(filepath))
+    {
+        fs::remove(filepath);
+        char response = '1';
+        SSL_write(ssl, &response, sizeof(response));
+        // Thông báo file bị xóa
+        cout << "File deleted: " << filepath << "\n";
+    }
+    else
+    {
+        char response = '0';
+        SSL_write(ssl, &response, sizeof(response));
+        // Thông báo file không tồn tại
+        cout << "Failed to delete file (not found): " << filepath << "\n";
+    }
 }
